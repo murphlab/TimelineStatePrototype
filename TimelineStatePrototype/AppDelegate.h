88d36d04420114ad40404d230f02564abc2d4422
Ken Murphy <kenmrph@gmail.com>
2016-06-22 14:35:29 -0700

    Initial Commit

---
+++ TimelineStatePrototype/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimelineStatePrototype
+//
+//  Created by Ken Murphy on 6/22/16.
+//  Copyright © 2016 Murphlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TimelineStatePrototype/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimelineStatePrototype
+//
+//  Created by Ken Murphy on 6/22/16.
+//  Copyright © 2016 Murphlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
