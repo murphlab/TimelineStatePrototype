a81543e10b3f4273d80232040c8893fe8fb865d4
Ken Murphy <kenmrph@gmail.com>
2016-06-22 14:39:15 -0700

    Stub class

---
+++ TimelineStatePrototype/TimelineStatePanel.h
@@ -0,0 +1,13 @@
+//
+//  TimelineStatePanel.h
+//  TimelineStatePrototype
+//
+//  Created by Ken Murphy on 6/22/16.
+//  Copyright © 2016 Murphlab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TimelineStatePanel : UIView
+
+@end
---
+++ TimelineStatePrototype/TimelineStatePanel.m
@@ -0,0 +1,21 @@
+//
+//  TimelineStatePanel.m
+//  TimelineStatePrototype
+//
+//  Created by Ken Murphy on 6/22/16.
+//  Copyright © 2016 Murphlab. All rights reserved.
+//
+
+#import "TimelineStatePanel.h"
+
+@implementation TimelineStatePanel
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
